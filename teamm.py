80ed4b8fa31da61980348e7b89b66b1c3ffe558b
Brandon20202020 <57911002+Brandon20202020@users.noreply.github.com>
2020-01-31 11:20:57 -0500

    Update teamm.py

--- teamm.py
+++ teamm.py
@@ -10,6 +10,33 @@ roomArray[304] = "There is a red door directly North. There is a red object in t
 roomArray[404] = "There is a red key on the floor. There is another room to the East and a hallway to the North."
 roomArray[303] = "The red door was opened. The walls in this room are painted red. There is a blue key in the room."
 
+roomArray[306] = "The room has black and white striped wallpaper. There is one dim light hanging from the ceiling. Towards the left corner you notice a small green key. There are no other exits besides the one you entered through, from the east."
+roomArray[405] = "There are walls to your East and West. There is a faint light coming from the South."
+roomArray[406] = "There are no lights in this room. However, a dim light eluminates the floor from your West. A stronger light eluminates the floor from your East."
+roomArray[506] = "The room is brightly lit. There are white walls blocking the North, East, and South. A small gold coin sits on the floor next to a broken table."
+
+def doesRoomExist(roomNumber):
+    if roomArray[roomNumber] == False:
+        print("You can't go there")
+        return False
+    else:
+        return True
+    try:
+        roomNumber
+    except:
+        print("You can't go there")
+        return False
+
+def move(userInput, location):
+    if userInput == "n" and doesRoomExist(location - 1) == True:
+        location = location - 1
+    elif userInput == "s" and doesRoomExist(location + 1) == True:
+        location = location + 1
+    elif userInput == "e" and doesRoomExist(location + 100) == True:
+        location = location + 100
+    elif userInput == "w" and doesRoomExist(location - 100) == True:
+        location = location - 100
+
 def main():
   location = roomArray[204]
   print("Corrdior of Secrets")
