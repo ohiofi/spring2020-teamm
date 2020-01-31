bc98039027edd33be98d926db316fd0329402101
matthewmclain <57911078+matthewmclain@users.noreply.github.com>
2020-01-31 11:36:22 -0500

    Update teamm.py

--- teamm.py
+++ teamm.py
@@ -11,6 +11,10 @@ roomArray[204] = "There is a long hallway to the East. There are marks on the wa
 roomArray[304] = "There is a red door directly North. There is a red object in the distance toward the East."
 roomArray[404] = "There is a red key on the floor. There is another room to the East and a hallway to the North."
 roomArray[303] = "The red door was opened. The walls in this room are painted red. There is a blue key in the room."
+roomArray[502] = "You are in a room with a shiny gold coin, to the south of you see a red key."
+roomArray[503] = "In the current room there is a gold coin to the north of you and a red key to your south."
+roomArray[504] = "There is a red key in the room with you, an a gold coin in the room to your right, and one in the distance to the north of you."
+roomArray[604] = " A gold coin is sitting in front of you in the small room."
 
 def main():
   location = roomArray[204]
